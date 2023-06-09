0dca9eab43445d032e9ea2da511e74ce98936d2e
ChiaraS97 <chiara.sucato97@gmail.com>
2023-06-09 15:35:01 +0200

    add TechnologySeeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $technologies = ['HTML', 'CSS', 'Vue', 'PHP', 'Laravel', 'MySQL'];
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology, '-');
+            $newTechnology->save();
+
+        }
+
+    }
+}
